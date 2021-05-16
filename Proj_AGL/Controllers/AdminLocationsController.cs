dffec6e7d71217e02f8f7b0b67f7ccad6fc55e4c
Seemon <seemon-s@live.com>
2021-05-16 20:22:43 +0300

    Add admin functionality

---
+++ Proj_AGL/Controllers/AdminLocationsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proj_AGL.Data;
+using Proj_AGL.Models;
+
+namespace Proj_AGL.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminLocationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminLocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminLocations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Locations.ToListAsync());
+        }
+
+        // GET: AdminLocations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // GET: AdminLocations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminLocations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,PositionX,PositionY")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(location);
+        }
+
+        // GET: AdminLocations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // POST: AdminLocations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,PositionX,PositionY")] Location location)
+        {
+            if (id != location.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(location.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(location);
+        }
+
+        // GET: AdminLocations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // POST: AdminLocations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Proj_AGL/Controllers/AdminRequestTypesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proj_AGL.Data;
+using Proj_AGL.Models;
+
+namespace Proj_AGL.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminRequestTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminRequestTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminRequestTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.RequestTypes.ToListAsync());
+        }
+
+        // GET: AdminRequestTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var requestType = await _context.RequestTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (requestType == null)
+            {
+                return NotFound();
+            }
+
+            return View(requestType);
+        }
+
+        // GET: AdminRequestTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminRequestTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] RequestType requestType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(requestType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requestType);
+        }
+
+        // GET: AdminRequestTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var requestType = await _context.RequestTypes.FindAsync(id);
+            if (requestType == null)
+            {
+                return NotFound();
+            }
+            return View(requestType);
+        }
+
+        // POST: AdminRequestTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] RequestType requestType)
+        {
+            if (id != requestType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(requestType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequestTypeExists(requestType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requestType);
+        }
+
+        // GET: AdminRequestTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var requestType = await _context.RequestTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (requestType == null)
+            {
+                return NotFound();
+            }
+
+            return View(requestType);
+        }
+
+        // POST: AdminRequestTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var requestType = await _context.RequestTypes.FindAsync(id);
+            _context.RequestTypes.Remove(requestType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequestTypeExists(int id)
+        {
+            return _context.RequestTypes.Any(e => e.Id == id);
+        }
+    }
+}
